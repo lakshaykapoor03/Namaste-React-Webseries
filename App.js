f91b867472b60b90694f3d8b2774e5567a6d1c77
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2023-04-04 12:50:36 +0530

    Episode 2 and 3

--- App.js
+++ App.js
@@ -1,4 +1,14 @@
-const heading = React.createElement("h1",{className:"heading"}, "Hello world from React!!");
-    const root = ReactDOM.createRoot(document.getElementById("root"))
-    root.render(heading)
-    console.log(heading)
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+
+const AppLayout = ()=>{
+    return(
+        <div className = "app">
+
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(<AppLayout/>)
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 Namaste React!!
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorith - writen in C++
+- Caching - Faster builds
