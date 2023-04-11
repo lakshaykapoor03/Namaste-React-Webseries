bf35fc3f41d29cdc47d43d24287d0151dadaba9e
lakshaykapoor03 <kapoorlakshay70@gmail.com>
2023-04-11 20:10:13 +0530

    gave routes to different components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 import Footer from "./Components/Footer";
+import Error from "./Components/Error";
+import AboutUs from "./Components/AboutUs";
+import Contact from "./Components/Contact";
+import Cart from "./Components/Cart";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet/>
+      <Footer />
+    </div>
+  );
+};
 
+const appRouter = createBrowserRouter([
+    { path: "/",
+element: <AppLayout />,
+errorElement:<Error/>, 
+children:[
+    { path: "/",
+    element: <Body /> },
+    { path: "/about",
+    element: <AboutUs /> },
+    { path: "/contact",
+    element: <Contact /> },
+    { path: "/cart",
+    element: <Cart /> }
+] } ]);
 
-const AppLayout = ()=>{
-    return(
-        <div className = "app">
-<Header/>
-<Body/>
-<Footer/>
-        </div>
-    )
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-    root.render(<AppLayout/>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
   return (
     <div className="flex justify-between p-2 bg-[gray] cursor-pointer">
-      <img className="w-12" src="https://icon-library.com/images/restaurant-app-icon/restaurant-app-icon-25.jpg" alt="" />
+      <Link to="/"><img className="w-12" src="https://icon-library.com/images/restaurant-app-icon/restaurant-app-icon-25.jpg" alt="" /></Link>
       <div  className= "flex w-[30%] justify-around list-none">
         
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact</li>
-          <li>Cart</li>
+        <Link to="/">  <li>Home</li></Link>
+        <Link to="/about"><li>About Us</li></Link>
+        <Link to="/contact"> <li>Contact</li></Link>
+        <Link to="/cart"><li>Cart</li></Link>
       
       </div>
     </div>
